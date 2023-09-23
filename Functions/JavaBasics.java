ceffbdaa7a47ac49a32430e93afc7dd2700858ec
MAYANK CHOUBEY <98317830+choubeyfactor@users.noreply.github.com>
2023-09-23 23:58:47 +0530

    Add files via upload

---
+++ Functions/JavaBasics.java
@@ -0,0 +1,16 @@
+public class JavaBasics {
+
+    //creating a function
+
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        return;   //writing a return statement
+    }
+    public static void main(String[] args) {
+        printHelloWorld(); //functioncall
+    }
+}
+
+//Those which are writtten under classes are called functions other than this all are called methods
